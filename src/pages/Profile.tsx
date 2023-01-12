03f1f26416a9a5faf726dd81cfe75b6be488e613
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-12 18:14:59 +0100

    Basic Profilepage design "done"

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,32 @@
 import "../styles/Profile.css";
 
 import React from "react";
+import { USERS } from "../data/faker";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="app-profile">
+      <img className="profile-img" src={USERS[0].avatar} alt="profile" />
+      <div className="profile-name">{USERS[0].fullname}</div>
+      <button>Edit profile</button>
+      <div>
+        <strong>Friends</strong>
+        <p>10 friends</p>
+      </div>
+      <div className="profile-friends">
+        {USERS.map((u) => {
+          return (
+            <div className="friend-container" key={u.userId}>
+              <img src={u.avatar} alt="" className="friend-img" />
+              <div className="friend-name">
+                <strong>{u.fullname}</strong>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
