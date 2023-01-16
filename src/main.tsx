ec1f817de963a1c39a3b34f758f5a0b3bb25d882
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-16 14:41:21 +0100

    Added paths and a temporary user check

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Friends from "./pages/Friends";
 import Home from "./pages/Home";
@@ -9,16 +9,23 @@ import Navbar from "./components/Navbar";
 import Profile from "./pages/Profile";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Register from "./pages/Register";
+
+// TEMPORARY!!!!!!!!!
+const user = {
+  authanticated: false,
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Navbar />
+      {user.authanticated ? <Navbar /> : null}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/friends" element={<Friends />} />
+        <Route path="/" element={user.authanticated ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={user.authanticated ? <Navigate to="/" /> : <Login />} />
+        <Route path="/profile" element={user.authanticated ? <Profile /> : <Navigate to="/login" />} />
+        <Route path="/friends" element={user.authanticated ? <Friends /> : <Navigate to="/login" />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
