ac115d952e6d26906e2bab1fe19ba456386ad862
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-21 13:07:51 +0100

    Added comment to not spam api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,25 @@ import "../styles/Home.css";
 import Post from "../components/Post";
 import React from "react";
 import { USERS } from "../data/faker";
+import { UserProp } from "../App";
 
-function Home() {
+function Home(props: UserProp) {
   return (
-    <div className="app-home">
-      {USERS.map((u) => {
-        return (
-          <>
-            <Post user={u} />
-            <div className="app-line"></div>
-          </>
-        );
-      })}
-      <div className="home-bottom" onClick={() => window.scrollTo(0, 0)}>
-        Back to top
+    <>
+      <div className="app-home">
+        {/* {USERS.map((u) => {
+          return (
+            <>
+              <Post user={u} />
+              <div className="app-line"></div>
+            </>
+          );
+        })} */}
+        <div className="home-bottom" onClick={() => window.scrollTo(0, 0)}>
+          Back to top
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
