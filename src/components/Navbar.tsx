9bab97c62ada3aa0405c4c14fd4d5574e0c78018
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-21 13:05:33 +0100

    Added logout functionallity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,16 @@ import "../styles/Navbar.css";
 
 import { Link } from "react-router-dom";
 import React from "react";
+import { SetUserStateProp } from "../App";
+import { deleteJwtToken } from "../auth";
 
 const fbColor = "rgb(57 117 234)";
 
-function Navbar() {
+function Navbar(props: SetUserStateProp) {
+  function handleLogout() {
+    deleteJwtToken();
+    props.setUser(null);
+  }
   return (
     <div className="app-navbar">
       <h1>odinbook</h1>
@@ -36,6 +42,16 @@ function Navbar() {
               />
             </svg>
           </Link>
+        </li>
+        <li>
+          <div onClick={handleLogout}>
+            <svg width="32px" height="32px" viewBox="0 0 24 24">
+              <path
+                fill={fbColor}
+                d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z"
+              />
+            </svg>
+          </div>
         </li>
       </ul>
       <div className="app-line"></div>
