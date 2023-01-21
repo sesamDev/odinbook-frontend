5b4ede557432f505a94e5863775d21677f008ec4
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-21 13:06:40 +0100

    Moved everything to App file

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,11 @@
 import "./index.css";
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
-import Friends from "./pages/Friends";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
-import Profile from "./pages/Profile";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Register from "./pages/Register";
-import { getCurrentUser } from "./auth";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {getCurrentUser() ? <Navbar /> : null}
-      <Routes>
-        <Route path="/" element={getCurrentUser() ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={getCurrentUser() ? <Navigate to="/" /> : <Login />} />
-        <Route path="/profile" element={getCurrentUser() ? <Profile /> : <Navigate to="/login" />} />
-        <Route path="/friends" element={getCurrentUser() ? <Friends /> : <Navigate to="/login" />} />
-        <Route path="/register" element={getCurrentUser() ? <Navigate to="/" /> : <Register />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
