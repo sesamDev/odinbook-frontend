0ebbf72d06ba00fb01458d19c8f7377da9bc0246
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-02-07 14:13:46 +0100

    Styling to comments

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,7 +1,10 @@
+import "../styles/Comment.css";
+
 import React, { useEffect, useState } from "react";
 
 import { CommentType } from "../types";
 
+//TODO: Fix timestamp format.
 interface IComment {
   comment: CommentType | undefined;
 }
@@ -10,14 +13,12 @@ function Comment(props: IComment) {
   const { comment } = props;
 
   return (
-    <div className="post-comments">
+    <div className="post-comment">
+      <img src="/" alt="avatar" />
       <div className="comment-card">
-        <img src="/" alt="avatar" />
-        <div>
-          <p>{comment?.author.first_name + " " + comment?.author.last_name}</p>
-          <p>{comment?.text}</p>
-          <p>{`${comment?.timestamp}`}</p>
-        </div>
+        <p className="author">{comment?.author.first_name + " " + comment?.author.last_name}</p>
+        <p className="text">{comment?.text}</p>
+        <p className="timestamp">{`${comment?.timestamp}`}</p>
       </div>
     </div>
   );
